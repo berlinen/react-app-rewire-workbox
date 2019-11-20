fd4d9fe5fc23b92f479372eec9747d0eff6decfc
berlinen <1292393108@qq.com>
2019-11-20 14:43:26 +0800

    🧚‍ 更新workbox缓存策略

--- public/custom-service-worker.js
+++ public/custom-service-worker.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 if (workbox) {
@@ -12,24 +13,56 @@ const bgSyncPlugin = new workbox.backgroundSync.Plugin('todoQueue', {
 
 workbox.routing.registerRoute(
   /\.(?:js|css|html)$/,
-  workbox.strategies.networkFirst()
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'static-cache',
+  })
 )
 
 workbox.routing.registerRoute(
   'http://localhost:3000',
-  workbox.strategies.networkFirst()
+  new workbox.strategies.NetworkFirst({
+    cacheName: 'root-cache',
+  })
 )
 
 workbox.routing.registerRoute(
-  'http://localhost:8000/todos',
-  workbox.strategies.networkFirst(),
-  'GET'
-)
+  // Cache CSS files.
+  /\.css$/,
+  // Use cache but update in the background.
+  new workbox.strategies.StaleWhileRevalidate({
+    // Use a custom cache name.
+    cacheName: 'css-cache',
+  })
+);
 
 workbox.routing.registerRoute(
-  'http://localhost:8000/todos',
-  workbox.strategies.networkFirst({
-    plugins: [bgSyncPlugin]
-  }),
-  'POST'
-)
+  // Cache image files.
+  /\.(?:png|jpg|jpeg|svg|gif)$/,
+  // Use the cache if it's available.
+  new workbox.strategies.CacheFirst({
+    // Use a custom cache name.
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Cache only 20 images.
+        maxEntries: 20,
+        // Cache for a maximum of a week.
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
+// workbox.routing.registerRoute(
+//   'http://localhost:8000/todos',
+//   workbox.strategies.networkFirst(),
+//   'GET'
+// )
+
+// workbox.routing.registerRoute(
+//   'http://localhost:8000/todos',
+//   workbox.strategies.networkFirst({
+//     plugins: [bgSyncPlugin]
+//   }),
+//   'POST'
+// )
